cde0c317a249b099c372c8308c521ad79c77c5e0
Yashpambhar <bca2022yash1701@tnraocollege.org>
2024-07-17 01:46:53 +0530

    Update reversing.py

--- reversing.py
+++ reversing.py
@@ -4,3 +4,6 @@ reversed_list = original_list[::-1]
 
 print("Original List:", original_list)
 print("Reversed List:", reversed_list)
+
+output :-
+![Screenshot 2024-07-17 014521](https://github.com/user-attachments/assets/f1073c1d-2003-439c-aadd-2cd3d5ed382d)
