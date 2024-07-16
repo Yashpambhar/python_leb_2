d472fc121e3a4de9e8ef07eef24fed110b759bf9
Yashpambhar <bca2022yash1701@tnraocollege.org>
2024-07-17 01:37:42 +0530

    Update findlength.py

--- findlength.py
+++ findlength.py
@@ -3,3 +3,6 @@ sample_list = [10, 20, 30, 40, 50]
 list_length = len(sample_list)
 
 print("Length of the list is:", list_length)
+
+output :-
+![Screenshot 2024-07-17 013555](https://github.com/user-attachments/assets/31b4588a-cacc-4832-ae96-4eb41ecca7b1)
