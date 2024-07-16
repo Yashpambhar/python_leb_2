4fbee279009288ade992fdc370aa1294f0b68ee5
Yashpambhar <bca2022yash1701@tnraocollege.org>
2024-07-17 01:27:44 +0530

    Update interchange.py

--- interchange.py
+++ interchange.py
@@ -10,3 +10,6 @@ def swapList(newList):
 newList = [12, 35, 9, 56, 24]
 
 print(swapList(newList))
+
+output :-
+![Screenshot 2024-07-17 012432](https://github.com/user-attachments/assets/50a30dd9-7369-4aac-b3c3-cf78b9d0a28b)
