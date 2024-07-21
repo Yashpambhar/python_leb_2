e6cd0e1a731762b2562885a34b84b7fb8eeaeec0
Yashpambhar <bca2022yash1701@tnraocollege.org>
2024-07-21 23:27:16 +0530

    Update readwrite.py

--- readwrite.py
+++ readwrite.py
@@ -6,3 +6,6 @@ with open('example.txt', 'w') as file:
 with open('example.txt', 'r') as file:
     content = file.read()
     print(content)
+    
+output :-
+![WhatsApp Image 2024-07-21 at 11 19 26 PM (1)](https://github.com/user-attachments/assets/7cb16a45-c927-42fd-ab77-227d6974f39e)
